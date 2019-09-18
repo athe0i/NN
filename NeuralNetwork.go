71550f896e9daefc043255da12842659edf5e48a
Stanislav Kalashnyk <stanislav.kalashnyk@gmail.com>
2019-09-18 12:47:35 +0300

    fix load/save functionality

--- NeuralNetwork.go
+++ NeuralNetwork.go
@@ -4,6 +4,8 @@ import (
   "gonum.org/v1/gonum/mat"
   "math"
   "gonum.org/v1/gonum/stat/distuv"
+  "fmt"
+  "os"
   )
 
 type NeuralNetwork struct {
@@ -70,92 +72,101 @@ func (net NeuralNetwork) Predict(inputData []float64) mat.Matrix {
 	return finalOutputs
 }
 
-
 func (net *NeuralNetwork) Train(inputData []float64, targetData []float64) {
-	// forward propagation - we need intermediate results for learning
-	inputs := mat.NewDense(len(inputData), 1, inputData)
+  inputs := mat.NewDense(len(inputData), 1, inputData)
   hiddenInputs := make([]mat.Matrix, len(net.hiddens))
-	hiddenInputs[0] = dot(net.hiddenWeights[0], inputs)
-	hiddenOutputs := apply(sigmoid, hiddenInputs[0])
+  hiddenOutputs := make([]mat.Matrix, len(net.hiddens))
 
-  outputs := make([]mat.Matrix, len(net.hiddens))
-  //copy(hiddenOutputs, outputs[0])
-  outputs[0] = apply(sigmoid, hiddenInputs[0])
+  hiddenInputs[0] = dot(net.hiddenWeights[0], inputs)
+  hiddenOutputs[0] = apply(sigmoid, hiddenInputs[0])
 
-  for i := 1; i < len(net.hiddenWeights); i++ {
-    hiddenInputs[i] = dot(net.hiddenWeights[i], hiddenOutputs)
-    hiddenOutputs = apply(sigmoid, hiddenInputs[i])
-    //copy(hiddenOutputs, outputs[i])
-    outputs[i] = apply(sigmoid, hiddenInputs[i])
+  for i := 1; i < len(net.hiddens); i++ {
+    hiddenInputs[i] = dot(net.hiddenWeights[i], hiddenOutputs[i-1])
+    hiddenOutputs[i] = apply(sigmoid, hiddenInputs[i])
   }
 
-	finalInputs := dot(net.outputWeights, hiddenOutputs)
-	finalOutputs := apply(sigmoid, finalInputs)
+  outputInputs := dot(net.outputWeights, hiddenOutputs[len(hiddenOutputs) - 1])
+  outputOutputs := apply(sigmoid, outputInputs)
 
-	// find errors
-	targets := mat.NewDense(len(targetData), 1, targetData)
-	outputErrors := subtract(targets, finalOutputs)
+  //backpropagate
+  targets := mat.NewDense(len(targetData), 1, targetData)
+  outputErrors := substract(targets, outputOutputs)
   hiddenErrors := dot(net.outputWeights.T(), outputErrors)
 
-	// backpropagate
-  //fmt.Println("Backprop output")
-	net.outputWeights = add(net.outputWeights,
+  net.outputWeights = add(net.outputWeights,
 		scale(net.learningRate,
-			dot(multiply(outputErrors, sigmoidPrime(finalOutputs)),
-				hiddenInputs[len(outputs) - 1].T()))).(*mat.Dense)
+			dot(multiply(outputErrors, sigmoidPrime(outputOutputs)),
+				hiddenOutputs[len(hiddenOutputs) - 1].T()))).(*mat.Dense)
 
-  if (len(net.hiddens) > 1) {
-    //fmt.Println("Backprop hidden except first")
-    for i := len(net.hiddenWeights) - 1; i > 0; i-- {
-      nextErrors := dot(net.hiddenWeights[i].T(), hiddenErrors)
-      net.hiddenWeights[i] = add(net.hiddenWeights[i],
-        scale(net.learningRate,
-          dot(multiply(hiddenErrors, sigmoidPrime(outputs[i])),
-            hiddenInputs[i-1].T()))).(*mat.Dense)
+  for i := len(net.hiddenWeights) - 1; i > 0; i-- {
+    net.hiddenWeights[i] = add(net.hiddenWeights[i],
+		  scale(net.learningRate,
+			  dot(multiply(hiddenErrors, sigmoidPrime(hiddenOutputs[i])),
+				  hiddenInputs[i].T()))).(*mat.Dense)
 
-      hiddenErrors = nextErrors
-    }
+    hiddenErrors = dot(net.hiddenWeights[i].T(), hiddenErrors)
   }
-
   net.hiddenWeights[0] = add(net.hiddenWeights[0],
     scale(net.learningRate,
-      dot(multiply(hiddenErrors, sigmoidPrime(outputs[0])),
+      dot(multiply(hiddenErrors, sigmoidPrime(hiddenOutputs[0])),
         inputs.T()))).(*mat.Dense)
 }
 
 func (net NeuralNetwork) Save() {
-	/*h, err := os.Create("data/hweights.model")
-	defer h.Close()
-	if err == nil {
-		net.hiddenWeights.MarshalBinaryTo(h)
-	}
-	o, err := os.Create("data/oweights.model")
+  path := fmt.Sprintf("data/%s", net.getDataDirName())
+  fmt.Println(path)
+  if _, err := os.Stat(path); os.IsNotExist(err) {
+    os.Mkdir(path, 0755)
+  }
+
+  for i,layerWeights := range net.hiddenWeights {
+    h, err := os.Create(fmt.Sprintf("%s/hweights%d.model", path, i))
+  	defer h.Close()
+  	if err == nil {
+  		layerWeights.MarshalBinaryTo(h)
+  	}
+  }
+
+	o, err := os.Create(fmt.Sprintf("%s/oweights.model", path))
 	defer o.Close()
 	if err == nil {
 		net.outputWeights.MarshalBinaryTo(o)
-	}*/
+	}
 }
 
 // load a neural network from file
 func (net NeuralNetwork) Load() {
-  /*fmt.Println("Loading")
-	h, err := os.Open("data/hweights.model")
-	defer h.Close()
-	if err == nil {
-		net.hiddenWeights.Reset()
-		net.hiddenWeights.UnmarshalBinaryFrom(h)
-	}
-	o, err := os.Open("data/oweights.model")
+  fmt.Println("Loading")
+  path := fmt.Sprintf("data/%s", net.getDataDirName())
+
+  for i,_ := range net.hiddens {
+    h, err := os.Open(fmt.Sprintf("%s/hweights%d.model", path, i))
+  	defer h.Close()
+  	if err == nil {
+  		net.hiddenWeights[i].Reset()
+  		net.hiddenWeights[i].UnmarshalBinaryFrom(h)
+  	}
+  }
+
+	o, err := os.Open(fmt.Sprintf("%s/oweights.model", path))
 	defer o.Close()
 	if err == nil {
 		net.outputWeights.Reset()
 		net.outputWeights.UnmarshalBinaryFrom(o)
 	}
 
-  fmt.Println(net.hiddenWeights)
-	return*/
+	return
 }
 
+func (net NeuralNetwork) getDataDirName() string {
+  dirName := fmt.Sprintf("%d-", net.inputs)
+
+  for _,layerNeurons := range net.hiddens {
+    dirName += fmt.Sprintf("%d-", layerNeurons)
+  }
+
+  return fmt.Sprintf("%s%d", dirName, net.outputs)
+}
 
 func sigmoid(r, c int, z float64) float64 {
 	return 1.0 / (1 + math.Exp(-1*z))
@@ -216,7 +227,7 @@ func add(m, n mat.Matrix) mat.Matrix {
 	return o
 }
 
-func subtract(m, n mat.Matrix) mat.Matrix {
+func substract(m, n mat.Matrix) mat.Matrix {
 	r, c := m.Dims()
 	o := mat.NewDense(r, c, nil)
 	o.Sub(m, n)
@@ -240,5 +251,5 @@ func sigmoidPrime(m mat.Matrix) mat.Matrix {
 		o[i] = 1
 	}
 	ones := mat.NewDense(rows, 1, o)
-	return multiply(m, subtract(ones, m)) // m * (1 - m)
+	return multiply(m, substract(ones, m)) // m * (1 - m)
 }
